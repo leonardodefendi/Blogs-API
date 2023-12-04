cf87e1bf4c3ccf1033ae211ec743f91642306ef4
leonardodefendi <leo_defendi@hotmail.com>
2023-12-04 14:54:46 -0300

    Finaliza requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+    underscored: true,
+  });
+  // fazer o associate com blogpost ainda
+  return User;
+};
