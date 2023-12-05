962fada89995e00751afb14658727286a0c9f901
leonardodefendi <leo_defendi@hotmail.com>
2023-12-05 10:48:06 -0300

    Refatora PostMiddlewares

--- src/middlewares/validatePostCategories.js
+++ src/middlewares/validatePostCategories.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { Category } = require('../models');
 
 const validateCategories = async (req, res, next) => {
@@ -12,12 +13,9 @@ const validateCategories = async (req, res, next) => {
 };
 const verifyCategoryExist = async (req, res, next) => {
   const { categoryIds } = req.body;
-  const categories = await Category.findAll();
-  const exists = categoryIds.filter((id) => {
-    const find = categories.some((category) => Number(category.id) === Number(id));
-    return find;
-  });
-  if (exists.length !== categoryIds.length || categoryIds.length === 0) {
+  const mapCategories = categoryIds.map((id) => ({ id }));
+  const teste = await Category.findAll({ where: { [Op.or]: mapCategories } });
+  if (teste.length !== categoryIds.length || categoryIds.length === 0) {
     return res.status(400).json({ message: 'one or more "categoryIds" not found' });
   }
   next();
--- src/services/search.service.js
+++ src/services/search.service.js
@@ -4,11 +4,15 @@ const { BlogPost, Category, User } = require('../models');
 const searchPostParam = async (param) => {
   const query = `%${param}%`;
   const posts = await BlogPost.findAll({
-    where: { [Sequelize.Op.or]: 
-      [{ title: { [Sequelize.Op.like]: query } }, { content: { [Sequelize.Op.like]: query } }] },
-    include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } }, { model: Category,
+    where: { 
+      [Sequelize.Op.or]: 
+      [{ title: { [Sequelize.Op.like]: query } }, { content: { [Sequelize.Op.like]: query } }],
+    },
+    include: 
+    [{ model: User, as: 'user', attributes: { exclude: ['password'] } }, { model: Category,
       as: 'categories',
-      through: { attributes: [] } },
+      through: { attributes: [] }, 
+    },
     ],
   });
   return posts;
